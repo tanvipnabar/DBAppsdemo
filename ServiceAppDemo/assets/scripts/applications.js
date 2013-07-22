1b29eeb392639ace605793062630667cd2228ae8
Kyle Rosenberg <kyle@appfirst.com>
2013-07-22 12:00:38 -0500

    added data functionality
    adding some data interaction and data fetching from the api. no
    layout/design added yet.

--- ServiceAppDemo/assets/scripts/applications.js
+++ ServiceAppDemo/assets/scripts/applications.js
@@ -10,17 +10,29 @@ var Applications = Backbone.Collection.extend({
 
 window.allApplications = new Applications;
 
+var theData = {nodes: []};
+
 var initApplications = function() {
 	$.ajax({
 		url: "https://wwws.appfirst.com/api/v3/applications/",
 		dataType: "jsonp",
 		crossDomain: true,
 		success: function(data) {
+			window.components = new Components();
+			var counter = 0;
 			_.each(data, function(app) {
-				var application = new Application(app);
-				allApplications.add(application);
-				console.log(application);
+				if (counter < 80) {
+					var application = new Application(app);
+					allApplications.add(application);
+					//console.log(application);
+					var component = new Component(app);
+					components.add(component);	
+					counter++;			
+				}
+				
 			});
+			theData.nodes = components.toJSON();
+			drawGraph(); //render.js
 		},
 		error: function(e) {
 			console.log(e);
@@ -28,3 +40,98 @@ var initApplications = function() {
 	});
 }
 
+var Component = Backbone.Model.extend({
+	defaults: {
+		id: -1,
+		name: 'component name',
+		health: 2,
+		healthParams: [	{
+											processID: 123,
+											attribute: "cpu",
+											lowThreshold: 20,
+											highThreshold: 80
+										},
+										{
+											processID: 456,
+											attribute: "memory",
+											lowThreshold: 1400000,
+											highThreshold: 2000000
+										}
+		]
+	}
+})
+
+var Components = Backbone.Collection.extend({
+	model: Component
+})
+
+function clicked(e) {
+	console.log(e);
+		$.ajax({
+			url: "https://wwws.appfirst.com/api/v3/applications/"+e+"/processes",
+			dataType: "jsonp",
+			crossDomain: true,
+			success: function(data) {
+				console.log(data);
+				$('#process-container').empty();
+				
+				var componentData = components.get(e);
+				
+				//component name
+				$('#process-container').append('<h2>'+componentData.get('name')+'</h2>');
+				
+				//HEALTH
+				$('#process-container').append('<p><b>Health: </b>'+componentData.get('health')+'</p>');
+				$('#process-container').append('<h3>Health parameters:</h3>');
+				var healths = componentData.get('healthParams');
+				console.log(healths);
+				_.each(healths, function(param) {
+					console.log(param);
+					$('#process-container').append('<li>'+param['attribute']+' for process id: '+param['processID']+'<br>');
+					$('#process-container').append('lower threshold: '+param['lowThreshold']+'<br>');
+					$('#process-container').append('upper threshold: '+param['highThreshold']+'</li><br><br>');
+				});
+
+				//PROCESSES
+				$('#process-container').append('<h3>Processes:</h3>');
+
+				$.each(data, function(i, process) {
+					$('#process-container').append(
+	            '<section class="section" id="'+process['uid']+'">\
+	              <p class="title" id="title'+process['uid']+'"><a href="#">'+process['name']+'</a></p>\
+	              <div class="content" data-slug="panel1">\
+	              <ul class="no-disc"></ul></div>\
+	            </section>'					
+						).hide().fadeIn('fast');
+					processData(process['uid']);
+				});
+			},
+			error: function(e) {
+				console.log(e);
+			}
+		});
+}
+
+function processData(theuid) {
+	var url = "https://wwws.appfirst.com/api/processes/"+theuid+"/data/";
+	$.ajax({
+		url: url,
+		dataType: "jsonp",
+		crossDomain: true,
+		success: function(data) {
+			//console.log(data);
+			$.each(data, function(i, details) {
+				//console.log(details);
+				_.each(details, function(i, datt) {
+					$('#'+theuid+' .content ul').append('<li><b>'+datt+': </b>'+details[datt]+'</li>');
+				});
+				//$('#'+theuid+' .content ul').append('<li><b>CPU: </b>'+details['cpu']+'</li>');
+				//$('#'+theuid+' .content ul').append('<li><b>Memory: </b>'+details['memory']+'</li>');
+				//$('#'+theuid+' .content ul').append('<li><b>Page faults: </b>'+details['page_faults']+'</li>');
+			});
+		},
+		error: function(e) {
+			console.log(e);
+		}
+	});
+}	
--- ServiceAppDemo/assets/scripts/render.js
+++ ServiceAppDemo/assets/scripts/render.js
@@ -1,16 +1,17 @@
-$(function() {
-    var width = 960,
-        height = 960;
+function drawGraph() {
+    var width = $(window).width()-50,
+        height = $(window).height()-100;
 
     var force = d3.layout.force()
-        .gravity(.1)
+        .gravity(1)
         .distance(100)
-        .charge(-1200)
+        .charge($(window).height() * -4 )
         .size([width, height]);
 
     var svg = d3.select("body").append("svg:svg")
         .attr("width", width)
-        .attr("height", height);
+        .attr("height", height)
+        .text("a simple tooltip");
 
     // Per-type markers, as they don't inherit styles.
     svg.append("svg:defs").selectAll("marker")
@@ -23,37 +24,45 @@ $(function() {
         .attr("markerWidth", 6)
         .attr("markerHeight", 6)
         .attr("orient", "auto")
-      .append("svg:path")
+        .append("svg:path")
         .attr("d", "M0,-5L10,0L0,5");
 
 
       d3.json("data/applications.json", function(graph, error) {
         force
-            .nodes(graph.nodes)
+            .nodes(theData.nodes)
             .links(graph.links)
             .start();
 
         var path = svg.append("svg:g").selectAll("path")
             .data(force.links())
-          .enter().append("svg:path")
+            .enter().append("svg:path")
             .attr("class", "link")
+            .style("stroke", '#008AB8')
+            .style("stroke-opacity", .2)
+            .style("stroke-width", 1.5)
             .attr("marker-end","url(#open)");
 
         var node = svg.append("svg:g").selectAll("node")
             .data(force.nodes())
-          .enter().append("g")
+          .enter().append("circle")
             .attr("class", "node")
+            .attr("class", function(d) { return ("node health"+d.health) })
             .attr("r", 6)
+            .attr("id", function(d) { return (d.id) })
+            .style("stroke-width", 20)
+            .style("stroke-opacity", 0)
+            .on("click", function(d) {clicked(d.id)})
             //.style("fill", function(d) { return color(d.group); })
             .call(force.drag);
 
-        node.append("image")
+        /*node.append("image")
             .attr("xlink:href", "http://openiconlibrary.sourceforge.net/gallery2/open_icon_library-full/icons/png/32x32/devices/computer-6.png")
             .attr("x", -10)
             .attr("y", -10)
             .attr("width", 20)
             .attr("height", 20);
-
+        */
 
         var text = svg.append("svg:g").selectAll("g")
           .data(force.nodes())
@@ -71,13 +80,19 @@ $(function() {
             .attr("dy", ".35em")
             .attr("fill", "#00000")
             .text(function(d) { return d.name; });
+        
 
+        svg.style("opacity", 1e-6)
+          .transition()
+            .duration(1000)
+            .style("opacity", 1);        
       // Use elliptical arc path segments to doubly-encode directionality.
         force.on("tick", function() {
           path.attr("d", function(d) {
             var dx = d.target.x - d.source.x,
                 dy = d.target.y - d.source.y,
                 dr = Math.sqrt(dx * dx + dy * dy);
+            //console.log("DX = " + dx + "\tDY = " + dy + "\tDR = " + dr);
             return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
           });
 
@@ -87,5 +102,5 @@ $(function() {
             return "translate(" + d.x + "," + d.y + ")";
           });
         });
-      });
-    });
+      });   
+    }
